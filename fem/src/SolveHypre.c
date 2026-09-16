bd50954e82d1eba1233336c1cf1e11ec116a40be
dancaseb <danielisreeves@gmail.com>
2026-09-16 14:14:02 +0300

    change relaxation and coarsening type for AMS

--- fem/src/SolveHypre.c
+++ fem/src/SolveHypre.c
@@ -1548,8 +1548,8 @@ void STDCALLBULL FC_FUNC(createhypreams,CREATEHYPREAMS)
 #if defined(HAVE_HYPRE_CUDA)
   // Interpolation type 0 is not supported on GPU, use 6 instead.
   // see Hypre BoomerAMG docs for more info
-   HYPRE_AMSSetAlphaAMGOptions(precond, 10, 1, 3, hypre_dppara[3], 6, 0);
-   HYPRE_AMSSetBetaAMGOptions(precond, 10, 1, 3, hypre_dppara[4], 6, 0);
+   HYPRE_AMSSetAlphaAMGOptions(precond, 8, 1, 18, hypre_dppara[3], 6, 0);
+   HYPRE_AMSSetBetaAMGOptions(precond, 8, 1, 18, hypre_dppara[4], 6, 0);
 #else
    HYPRE_AMSSetAlphaAMGOptions(precond, 10, 1, 3, hypre_dppara[3], 0, 0);
    HYPRE_AMSSetBetaAMGOptions(precond, 10, 1, 3, hypre_dppara[4], 0, 0);
